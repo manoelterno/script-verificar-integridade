3433218c3e5e3840b9fa9f7ba3161dc14fa89efa
manoelterno <149274259+manoelterno@users.noreply.github.com>
2024-12-26 19:26:37 -0300

    Add files via upload

--- integridade.sh
+++ integridade.sh
@@ -1,54 +1,40 @@
 #!/bin/bash
 
 criar(){
-#limpando e criando  hash.txt#
-> ./hash.txt
-
-quantArquivos=$(ls $diretorio -1 |  wc -l)
-i=1
-
-while [ $i -le $quantArquivos ]
-do
- arquivo=$(ls $diretorio -l | tail -n +2 | awk -F" " '{print $9}' | head -$i | tail +$i)
- md5=$(md5sum $diretorio/$arquivo | awk -F" " '{print $2":"$1}')
- data=$(date +"%d/%m/%Y")
- echo "$md5:$data" >> ./hash.txt
- i=$((i+1))
-done
+  for arquivo in $(find $diretorio -type f ); do
+  md5=$(md5sum $arquivo | awk -F" " '{print $1}')
+  echo "$arquivo:$md5" >> ./hash.txt
+  done
 }
 
-verificar() {
-quantArquivos=$(ls $diretorio -1 |  wc -l)
-i=1
-while [ $i -le $quantArquivos ]
-do
- arquivo=$(ls $diretorio -l | tail -n +2 | awk -F" " '{print $9}' | head -$i | tail +$i)
- hashOriginal=$(cat ./hash.txt | head -$i | tail +$i | awk -F":" '{print $2}')
- hashAtual=$(md5sum $diretorio/$arquivo | awk -F" " '{print $1}')
- if [ $hashAtual == $hashOriginal ]; then
-    i=$((i+1))
-    continue
- else
-    #divergencia encontrada
-    data=$(date +"%d/%m/%Y")
-    novaLinha="$diretorio/$arquivo:$hashAtual:$data"
-    sed -i "/$arquivo/c\\$novaLinha" hash.txt
- fi
- i=$((i+1))
-done
+verificar(){
+  for arquivo in $(find $diretorio -type f ); do
+    if [ $(grep -ic $arquivo ./hash.txt) -eq 1 ] 
+    then
+      
+      hashOriginal=$(cat hash.txt | grep $arquivo | awk -F":" '{print $2}')
+      hashAtual=$(md5sum $arquivo | awk -F" " '{print $1}')
+      
+      arquivo_escapado=$(echo "$arquivo" | sed 's/[\/&]/\\&/g')
+
+      if [ "$hashAtual" != "$hashOriginal" ]
+      then
+        sed -i "s/$arquivo_escapado:$hashOriginal/$arquivo_escapado:$hashAtual/" hash.txt
+        echo $(grep $arquivo hash.txt)
+      fi
+    else
+      md5=$(md5sum $arquivo | awk -F" " '{print $1}')
+      echo "$arquivo:$md5" >> ./hash.txt
+    fi
+  done
 }
 
-
-
-#substituir diretorio/ por diretorio
-diretorio=${1///}
+diretorio=$1
 
 FILE=./hash.txt
 if [[ -f $FILE ]]; then
-  echo "arquivo existe"
   verificar
 else
-  echo "arquivo nao existe"
   criar
 fi
 
