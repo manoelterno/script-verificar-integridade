724f901f01e009282c3617a95a3e81da4c26c723
manoelterno <149274259+manoelterno@users.noreply.github.com>
2025-02-24 17:02:02 -0300

    Update integridade.sh

--- integridade.sh
+++ integridade.sh
@@ -1,40 +1,44 @@
 #!/bin/bash
-criar(){
-  for arquivo in $(find $diretorio -type f ); do
-  md5=$(md5sum $arquivo | awk -F" " '{print $1}')
-  echo "$arquivo:$md5" >> $arquivo_hash
-  done
-  chmod 600 "$arquivo_hash"
+
+criar() {
+    while IFS= read -r arquivo; do
+        md5=$(md5sum "$arquivo" | awk -F" " '{print $1}')
+        echo "$arquivo:$md5" >> "$arquivo_hash"
+    done < <(find "$diretorio" -type f)
+    
+    chmod 600 "$arquivo_hash"
 }
-verificar(){
-  for arquivo in $(find $diretorio -type f ); do
-    if [ $(grep -ic $arquivo $arquivo_hash) -eq 1 ] 
-    then
-      hashOriginal=$(cat $arquivo_hash | grep $arquivo | awk -F":" '{print $2}')
-      hashAtual=$(md5sum $arquivo | awk -F" " '{print $1}')
-      arquivo_escapado=$(echo "$arquivo" | sed 's/[\/]/\\&/g')
-      if [ "$hashAtual" != "$hashOriginal" ]
-      then
-        sed -i "s/$arquivo_escapado:$hashOriginal/$arquivo_escapado:$hashAtual/" $arquivo_hash
-        echo $(grep $arquivo $arquivo_hash)
-      fi
-    else
-      md5=$(md5sum $arquivo | awk -F" " '{print $1}')
-      echo "$arquivo:$md5" >> $arquivo_hash
-    fi
-  done
-  for linha in $(cat $arquivo_hash); do
-    arquivo_temp=$(echo "$linha" | sed 's/\(.*\):.*/\1/')
-    if [ $(find $diretorio | grep -ic $arquivo_temp) -ne 1 ]; then
-      echo "$linha"
-    fi
-  done
+
+verificar() {
+    while IFS= read -r arquivo; do
+        if [ $(grep -ic "$arquivo" "$arquivo_hash") -eq 1 ]; then
+            hashOriginal=$(grep "$arquivo" "$arquivo_hash" | awk -F":" '{print $2}')
+            hashAtual=$(md5sum "$arquivo" | awk -F" " '{print $1}')
+            arquivo_escapado=$(echo "$arquivo" | sed 's/[\/]/\\&/g')
+
+            if [ "$hashAtual" != "$hashOriginal" ]; then
+                sed -i "s/$arquivo_escapado:$hashOriginal/$arquivo_escapado:$hashAtual/" "$arquivo_hash"
+                echo "$(grep "$arquivo" "$arquivo_hash")"
+            fi
+        else
+            md5=$(md5sum "$arquivo" | awk -F" " '{print $1}')
+            echo "$arquivo:$md5" >> "$arquivo_hash"
+        fi
+    done < <(find "$diretorio" -type f)
+
+    while IFS= read -r linha; do
+        arquivo_temp=$(echo "$linha" | sed 's/\(.*\):.*/\1/')
+        if [ $(find "$diretorio" | grep -ic "$arquivo_temp") -ne 1 ]; then
+            echo "$linha"
+        fi
+    done < <(cat "$arquivo_hash")
 }
+
 diretorio=${1:? Necessario fornecer um diretorio}
 arquivo_hash="./hash_$(echo "$diretorio" | sed 's/[\/]/_/g').txt"
-if [[ -f $arquivo_hash ]]; then
-  verificar
-else
-  criar
-fi
 
+if [[ -f $arquivo_hash ]]; then
+    verificar
+else
+    criar
+fi
